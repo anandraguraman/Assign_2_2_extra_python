53739dbcf493801651e58833274b4196b67086f5
anandraguraman <113701095+anandraguraman@users.noreply.github.com>
2022-10-10 18:16:48 -0400

    Add files via upload

---
+++ python_emotion_badge.py
@@ -0,0 +1,9 @@
+# Imports go at the top
+from microbit import *
+
+while True:
+    if button_a.is_pressed():
+        display.show(Image.HAPPY)
+    if button_b.is_pressed():
+        display.show(Image.SAD)
+        
